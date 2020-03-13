5c985e8899f24f5bb1ae6409bdf12caee9c34366
jun.hu <jun.hu@picahealth.com>
2020-03-13 14:24:04 +0800

    'changes'

---
+++ src/main/java/com/coding/training/algorithmic/num0003.java
@@ -0,0 +1,15 @@
+package com.coding.training.algorithmic;
+
+/**
+ * 面试题05. 替换空格
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * 示例 1：
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ * 限制：
+ * 0 <= s 的长度 <= 10000
+ * 通过次数12,040提交次数15,437
+ * https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+ */
+public class num0003 {
+}
